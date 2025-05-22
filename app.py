30b66e6c40ce20186d1b5aa5e35c869766a97668
Daniel0227800 <varguitaspipe69@gmail.com>
2025-05-22 09:14:19 -0500

    fix: usar host y puerto correctos para Render

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def home():
     return "Hola, esta API usa CI/CD con GitHub Actions 🚀"
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
